918c413e531f7d443373a5253e6a4d3b002eaf2c
Mikaela-D <G00401392@atu.ie>
2025-02-07 20:16:29 +0000

    Removed unnecessary code

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -11,38 +11,6 @@ function MainNavigation() {
   const globalCtx = useContext(GlobalContext);
   const router = useRouter();
 
-  // Timer State
-  const [timeLeft, setTimeLeft] = useState(null);
-
- 
-  const SALE_DURATION_HOURS = 48; 
-  
-  const SALE_END_TIME = useRef(new Date().getTime() + SALE_DURATION_HOURS * 60 * 60 * 1000);
-
-  useEffect(() => {
-    function updateCountdown() {
-      const now = new Date().getTime();
-      const difference = SALE_END_TIME.current - now; 
-
-      if (difference <= 0) {
-        setTimeLeft("Sale Ended!");
-        clearInterval(timerInterval); // Stop the timer when it ends
-      } else {
-        const hours = Math.floor(difference / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
-      }
-    }
-
-    // Update every second
-    const timerInterval = setInterval(updateCountdown, 1000);
-    updateCountdown(); 
-
-    // Cleanup on component unmount
-    return () => clearInterval(timerInterval);
-  }, []);
-
   function toggleMenuHide() {
     globalCtx.updateGlobals({ cmd: "hideHamMenu", newVal: false });
   }
@@ -60,16 +28,12 @@ function MainNavigation() {
       <HamMenuContent contents={contents} />
       <HamMenu toggleMenuHide={() => toggleMenuHide()} />
       <HamMenuFAB toggleMenuHide={() => toggleMenuHide()} />
-      
-      
-      <div className={classes.saleTimer}>
-        <p>🎅 Christmas Sale Ends In: {timeLeft ? timeLeft : "Loading..."}</p>
-      </div>
 
       <nav>
         <ul>
           <li>
-            <Link href="/">All Products</Link> ({globalCtx.theGlobalObject.meetings.length})
+            <Link href="/">All Products</Link> (
+            {globalCtx.theGlobalObject.meetings.length})
           </li>
           <li>
             <Link href="/new-meetup">Add New Product</Link>
