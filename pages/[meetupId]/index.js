7a9f233a71fd9e13895afe162eb9cdb758b096fc
Mikaela-D <G00401392@atu.ie>
2025-02-22 21:38:45 +0000

    Added new parameters here too

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,20 +1,32 @@
-import MeetupDetail from '../../components/meetups/MeetupDetail'
-import { useRouter } from 'next/router'
-import GlobalContext from "../../pages/store/globalContext"
-import { useContext } from 'react'
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { useRouter } from "next/router";
+import GlobalContext from "../../pages/store/globalContext";
+import { useContext } from "react";
 
 export default function () {
-    const globalCtx = useContext(GlobalContext)
-    const router = useRouter();
+  const globalCtx = useContext(GlobalContext);
+  const router = useRouter();
 
-    // Back to basics, a simple for loop. Also trim() comes into play as it usually does!
-    let returnVal = null
-    for (let ii = 0; ii < globalCtx.theGlobalObject.meetings.length; ii++) {
-        let temp = globalCtx.theGlobalObject.meetings[ii]
-        if (temp.meetingId.trim() == router.query.meetupId.trim()) {
-            returnVal = <MeetupDetail image={temp.image} title={temp.title} price={temp.price} category={temp.category} description={temp.description} />
-        }
+  // Back to basics, a simple for loop. Also trim() comes into play as it usually does!
+  let returnVal = null;
+  for (let ii = 0; ii < globalCtx.theGlobalObject.meetings.length; ii++) {
+    let temp = globalCtx.theGlobalObject.meetings[ii];
+    if (temp.meetingId.trim() == router.query.meetupId.trim()) {
+      returnVal = (
+        <MeetupDetail
+          image={temp.image}
+          title={temp.title}
+          customerName={temp.customerName}
+          customerPhone={temp.customerPhone}
+          customerEmail={temp.customerEmail}
+          category={temp.category}
+          priority={temp.priority}
+          status={temp.status}
+          description={temp.description}
+        />
+      );
     }
-    // In the real world, we'd put the code above in the store context module. 
-    return returnVal
+  }
+  // In the real world, we'd put the code above in the store context module.
+  return returnVal;
 }
