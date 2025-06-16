180f4535d4fa927485b8adb63c1afa53e73ff262
Mikaela-D <G00401392@atu.ie>
2025-06-16 19:16:35 +0100

    Show the main navigation only when I am logged in

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,20 @@
 import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
 
 function Layout(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsLoggedIn(localStorage.getItem("isLoggedIn") === "true");
+    }
+  }, []);
+
   return (
     <div className={classes.layout}>
-      <MainNavigation />
+      {isLoggedIn && <MainNavigation />}
       <main className={classes.main}>{props.children}</main>
       <Footer />
     </div>
