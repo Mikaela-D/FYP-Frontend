872f4ea6a660b63036165456dcc05b3631bfdfd3
Mikaela-D <G00401392@atu.ie>
2025-06-29 22:27:10 +0100

    Add NewCallPage component with CallControls integration

---
+++ pages/new-call/index.js
@@ -0,0 +1,10 @@
+import CallControls from "../call-controls/index";
+
+export default function NewCallPage() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>New Call</h1>
+      <CallControls />
+    </div>
+  );
+}
